7ea1fad5ee0d3593d8452c80d5df1b14436fcb5d
Jagucan <jjgc.2499@gmail.com>
2023-12-29 10:35:02 -0500

    create teleport v1

--- Assets/Scripts/TeleportPoint.cs
+++ Assets/Scripts/TeleportPoint.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TeleportPoint : MonoBehaviour
 {
+    public UnityEvent OnTeleportEnter;
+    public UnityEvent OnTeleport;
+    public UnityEvent OnTeleportExit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.GetChild(0).gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTeleportEnterXR()
     {
-        
+        OnTeleportEnter?.Invoke();
+    }
+
+    public void OnTeleportClickXR()
+    {
+        ExecuteTeleoportation();
+        OnTeleport?.Invoke();
+        TeleportManager.Instance.DisableTeleportPoint(gameObject);
+    }
+
+    public void OnTeleportExitXR()
+    {
+        OnTeleportExit?.Invoke();
+    }
+
+    private void ExecuteTeleoportation()
+    {
+        GameObject player = TeleportManager.Instance.Player;
+        player.transform.position = transform.position;
+        Camera camera = player.GetComponentInChildren<Camera>();
+        float rotationY = transform.rotation.eulerAngles.y - camera.transform.localEulerAngles.y;
+        player.transform.rotation = Quaternion.Euler(0, rotationY, 0);
     }
 }
